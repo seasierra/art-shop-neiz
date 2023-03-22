936a3d934448c9516e4ac07edf85fb389e24a6d7
seasierra <125124420+seasierra@users.noreply.github.com>
2023-03-22 02:03:18 +0000

    test seo

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,5 +1,5 @@
 import commerce from '@lib/api/commerce'
-import { Layout } from '@components/common'
+import { Layout, SEO } from '@components/common'
 import { ProductCard } from '@components/product'
 import { Grid, Marquee, Hero } from '@components/ui'
 // import HomeAllProductsGrid from '@components/common/HomeAllProductsGrid'
@@ -84,6 +84,17 @@ export default function Home({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
+      {/* <SEO
+        title="Contemporary artist Neiz.vesten"
+        description="All the art that you will see here is a tool for understanding the world and yourself for the artist and the people with whom he intersects."
+        openGraph={{
+          images: [
+            {
+              url: 'https://neizvesten.com/assets/img/social.jpg',
+            },
+          ],
+        }}
+      /> */}
       <AdaptiveVideoPlayer videoSrc="media/main_ibrzrf" />
       {/* <Grid variant="filled">
         {products.slice(0, 3).map((product: any, i: number) => (
