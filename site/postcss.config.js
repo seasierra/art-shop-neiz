5527f8cc17b187ba1803e570a325b91163c77cfb
seasierra <125124420+seasierra@users.noreply.github.com>
2023-03-22 22:05:28 +0000

    add purge css

--- site/postcss.config.js
+++ site/postcss.config.js
@@ -16,5 +16,16 @@ module.exports = {
         },
       },
     ],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: [
+          './pages/**/*.{js,jsx,ts,tsx}',
+          './components/**/*.{js,jsx,ts,tsx}',
+        ],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: ['html', 'body'],
+      },
+    ],
   ],
 }
