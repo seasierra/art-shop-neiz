d0a67aef25ce2f57f5a62f9eb2209bbf7dc2493d
VladyslavKovalov <chadjou@gmail.com>
2023-05-09 18:48:07 +0300

    Revert "add purge css"
    This reverts commit 5527f8cc17b187ba1803e570a325b91163c77cfb.

--- site/postcss.config.js
+++ site/postcss.config.js
@@ -16,16 +16,5 @@ module.exports = {
         },
       },
     ],
-    [
-      '@fullhuman/postcss-purgecss',
-      {
-        content: [
-          './pages/**/*.{js,jsx,ts,tsx}',
-          './components/**/*.{js,jsx,ts,tsx}',
-        ],
-        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-        safelist: ['html', 'body'],
-      },
-    ],
   ],
 }
